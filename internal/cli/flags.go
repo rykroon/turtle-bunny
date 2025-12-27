fb6471ea5f38477e991c3c65ba835d9ce3b418e4
Ryan Kroon <rykroon.tech@gmail.com>
2025-12-26 22:06:40 -0500

    use the turtlebunny alias of uint128 in the flag logic.

--- internal/cli/flags.go
+++ internal/cli/flags.go
@@ -4,23 +4,23 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/rykroon/turtlebunny"
 	"github.com/spf13/pflag"
-	"lukechampine.com/uint128"
 )
 
-type uint128Flag uint128.Uint128
+type uint128Flag turtlebunny.Uint128
 
-func newUint128Flag(val uint128.Uint128, p *uint128.Uint128) *uint128Flag {
+func newUint128Flag(val turtlebunny.Uint128, p *turtlebunny.Uint128) *uint128Flag {
 	*p = val
 	return (*uint128Flag)(p)
 }
 
 func (u uint128Flag) String() string {
-	return uint128.Uint128(u).String()
+	return turtlebunny.Uint128(u).String()
 }
 
 func (uf *uint128Flag) Set(s string) error {
-	u, err := uint128.FromString(s)
+	u, err := turtlebunny.NewUint128FromString(s)
 	if err != nil {
 		return err
 	}
@@ -32,32 +32,32 @@ func (u uint128Flag) Type() string {
 	return "uint128"
 }
 
-func Uint128VarP(f *pflag.FlagSet, p *uint128.Uint128, name, shorthand string, value uint128.Uint128, usage string) {
+func Uint128VarP(f *pflag.FlagSet, p *turtlebunny.Uint128, name, shorthand string, value turtlebunny.Uint128, usage string) {
 	f.VarP(newUint128Flag(value, p), name, shorthand, usage)
 }
 
-func Uint128Var(f *pflag.FlagSet, p *uint128.Uint128, name string, value uint128.Uint128, usage string) {
+func Uint128Var(f *pflag.FlagSet, p *turtlebunny.Uint128, name string, value turtlebunny.Uint128, usage string) {
 	Uint128VarP(f, p, name, "", value, usage)
 }
 
-func Uint128P(f *pflag.FlagSet, name, shorthand string, value uint128.Uint128, usage string) {
-	p := new(uint128.Uint128)
+func Uint128P(f *pflag.FlagSet, name, shorthand string, value turtlebunny.Uint128, usage string) {
+	p := new(turtlebunny.Uint128)
 	Uint128VarP(f, p, name, shorthand, value, usage)
 }
 
-func Uint128(f *pflag.FlagSet, name string, value uint128.Uint128, usage string) {
-	p := new(uint128.Uint128)
+func Uint128(f *pflag.FlagSet, name string, value turtlebunny.Uint128, usage string) {
+	p := new(turtlebunny.Uint128)
 	Uint128VarP(f, p, name, "", value, usage)
 }
 
 // ~~~~~~~~~~~~~~~~
 
 type uint128SliceValue struct {
-	value   *[]uint128.Uint128
+	value   *[]turtlebunny.Uint128
 	changed bool
 }
 
-func newUint128SliceValue(val []uint128.Uint128, p *[]uint128.Uint128) *uint128SliceValue {
+func newUint128SliceValue(val []turtlebunny.Uint128, p *[]turtlebunny.Uint128) *uint128SliceValue {
 	isv := new(uint128SliceValue)
 	isv.value = p
 	*isv.value = val
@@ -66,14 +66,13 @@ func newUint128SliceValue(val []uint128.Uint128, p *[]uint128.Uint128) *uint128S
 
 func (s *uint128SliceValue) Set(val string) error {
 	ss := strings.Split(val, ",")
-	out := make([]uint128.Uint128, len(ss))
+	out := make([]turtlebunny.Uint128, len(ss))
 	for i, d := range ss {
 		var err error
-		out[i], err = uint128.FromString(d)
+		out[i], err = turtlebunny.NewUint128FromString(d)
 		if err != nil {
 			return err
 		}
-
 	}
 	if !s.changed {
 		*s.value = out
@@ -98,26 +97,26 @@ func (s *uint128SliceValue) String() string {
 
 // UintSliceVar defines a uintSlice flag with specified name, default value, and usage string.
 // The argument p points to a []uint variable in which to store the value of the flag.
-func Uint128SliceVar(f *pflag.FlagSet, p *[]uint128.Uint128, name string, value []uint128.Uint128, usage string) {
+func Uint128SliceVar(f *pflag.FlagSet, p *[]turtlebunny.Uint128, name string, value []turtlebunny.Uint128, usage string) {
 	f.VarP(newUint128SliceValue(value, p), name, "", usage)
 }
 
 // UintSliceVarP is like UintSliceVar, but accepts a shorthand letter that can be used after a single dash.
-func Uint128SliceVarP(f *pflag.FlagSet, p *[]uint128.Uint128, name, shorthand string, value []uint128.Uint128, usage string) {
+func Uint128SliceVarP(f *pflag.FlagSet, p *[]turtlebunny.Uint128, name, shorthand string, value []turtlebunny.Uint128, usage string) {
 	f.VarP(newUint128SliceValue(value, p), name, shorthand, usage)
 }
 
 // UintSlice defines a []uint flag with specified name, default value, and usage string.
 // The return value is the address of a []uint variable that stores the value of the flag.
-func Uint128Slice(f *pflag.FlagSet, name string, value []uint128.Uint128, usage string) *[]uint128.Uint128 {
-	p := []uint128.Uint128{}
+func Uint128Slice(f *pflag.FlagSet, name string, value []turtlebunny.Uint128, usage string) *[]turtlebunny.Uint128 {
+	p := []turtlebunny.Uint128{}
 	Uint128SliceVarP(f, &p, name, "", value, usage)
 	return &p
 }
 
 // UintSliceP is like UintSlice, but accepts a shorthand letter that can be used after a single dash.
-func Uint128SliceP(f *pflag.FlagSet, name, shorthand string, value []uint128.Uint128, usage string) *[]uint128.Uint128 {
-	p := []uint128.Uint128{}
+func Uint128SliceP(f *pflag.FlagSet, name, shorthand string, value []turtlebunny.Uint128, usage string) *[]turtlebunny.Uint128 {
+	p := []turtlebunny.Uint128{}
 	Uint128SliceVarP(f, &p, name, shorthand, value, usage)
 	return &p
 }
--- uint128.go
+++ uint128.go
@@ -1,5 +1,11 @@
 package turtlebunny
 
-import "lukechampine.com/uint128"
+import (
+	"lukechampine.com/uint128"
+)
 
 type Uint128 = uint128.Uint128
+
+func NewUint128FromString(s string) (Uint128, error) {
+	return uint128.FromString(s)
+}
