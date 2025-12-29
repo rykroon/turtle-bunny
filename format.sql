921b81ad43bd3803e1253bcb25352489c71f5128
Ryan Kroon <rykroon.tech@gmail.com>
2025-12-29 13:52:21 -0500

    removed check for text uint being greater than 0. regex pattern already ensures that.

--- format.sql
+++ format.sql
@@ -1,27 +1,22 @@
 CREATE TABLE IF NOT EXISTS accounts (
     id TEXT PRIMARY KEY CHECK (
         id REGEXP '^(0|[1-9][0-9]*)$' AND
-        decimal_cmp(id, '0') IN (0, 1) AND
         decimal_cmp(id, '340282366920938463463374607431768211455') IN (-1, 0)
     ),
     debits_posted TEXT NOT NULL DEFAULT 0 CHECK (
         debits_posted REGEXP '^(0|[1-9][0-9]*)$' AND
-        decimal_cmp(debits_posted, '0') IN (0, 1) AND
         decimal_cmp(debits_posted, '340282366920938463463374607431768211455') IN (-1, 0)
     ),
     credits_posted TEXT NOT NULL DEFAULT 0 CHECK (
         credits_posted REGEXP '^(0|[1-9][0-9]*)$' AND
-        decimal_cmp(credits_posted, '0') IN (0, 1) AND
         decimal_cmp(credits_posted, '340282366920938463463374607431768211455') IN (-1, 0)
     ),
     user_data_128 TEXT NOT NULL CHECK (
         user_data_128 REGEXP '^(0|[1-9][0-9]*)$' AND
-        decimal_cmp(user_data_128, '0') IN (0, 1) AND
         decimal_cmp(user_data_128, '340282366920938463463374607431768211455') IN (-1, 0)
     ),
     user_data_64 TEXT NOT NULL CHECK (
         user_data_128 REGEXP '^(0|[1-9][0-9]*)$' AND
-        decimal_cmp(user_data_128, '0') IN (0, 1) AND
         decimal_cmp(user_data_128, '18446744073709551615') IN (-1, 0)
     ),
     user_data_32 INTEGER NOT NULL CHECK (user_data_32 BETWEEN 0 AND 4294967295),
@@ -29,9 +24,8 @@ CREATE TABLE IF NOT EXISTS accounts (
     code INTEGER NOT NULL CHECK (code BETWEEN 0 AND 65535),
     debits_must_not_exceed_credits INTEGER NOT NULL CHECK (debits_must_not_exceed_credits IN (0,1)),
     credits_must_not_exceed_debits INTEGER NOT NULL CHECK (credits_must_not_exceed_debits IN (0,1)),
-    timestamp TEXT NOT NULL UNIQUE DEFAULT ((decimal(unixepoch('subsec') * 1000000))) CHECK (
+    timestamp TEXT NOT NULL UNIQUE DEFAULT ((decimal_mul(unixepoch('subsec'), 1000000))) CHECK (
         timestamp REGEXP '^(0|[1-9][0-9]*)$' AND
-        decimal_cmp(timestamp, '0') IN (0, 1) AND
         decimal_cmp(timestamp, '18446744073709551615') IN (-1, 0)
         -- make sure timestamp is less than or equal to current time
     )
@@ -74,8 +68,8 @@ BEGIN
             (OLD.credits_must_not_exceed_debits != NEW.credits_must_not_exceed_debits) OR
             (OLD.timestamp != NEW.timestamp)
         THEN RAISE(ABORT, "account_cannot_be_modified")
-        WHEN NEW.credits_must_not_exceed_debits AND NEW.credits_posted > NEW.debits_posted
-        THEN RAISE(ABORT, "exceeds_debits")
+        WHEN NEW.debits_must_not_exceed_credits AND NEW.debits_posted > NEW.credits_posted
+        THEN RAISE(ABORT, "exceeds_credits")
         WHEN NEW.credits_must_not_exceed_debits AND NEW.credits_posted > NEW.debits_posted
         THEN RAISE(ABORT, "exceeds_debits")
     END;
@@ -90,40 +84,33 @@ END;
 CREATE TABLE IF NOT EXISTS transfers (
     id TEXT PRIMARY KEY CHECK (
         id REGEXP '^(0|[1-9][0-9]*)$' AND
-        decimal_cmp(id, '0') IN (0, 1) AND
         decimal_cmp(id, '340282366920938463463374607431768211455') IN (-1, 0)
     ),
     debit_account_id TEXT NOT NULL CHECK (
         debit_account_id REGEXP '^(0|[1-9][0-9]*)$' AND
-        decimal_cmp(debit_account_id, '0') IN (0, 1) AND
         decimal_cmp(debit_account_id, '340282366920938463463374607431768211455') IN (-1, 0)
     ),
     credit_account_id TEXT NOT NULL CHECK (
         credit_account_id REGEXP '^(0|[1-9][0-9]*)$' AND
-        decimal_cmp(credit_account_id, '0') IN (0, 1) AND
         decimal_cmp(credit_account_id, '340282366920938463463374607431768211455') IN (-1, 0)
     ),
     amount TEXT NOT NULL CHECK (
         amount REGEXP '^(0|[1-9][0-9]*)$' AND
-        decimal_cmp(amount, '0') IN (0, 1) AND
         decimal_cmp(amount, '340282366920938463463374607431768211455') IN (-1, 0)
     ),
     user_data_128 TEXT NOT NULL CHECK (
         user_data_128 REGEXP '^(0|[1-9][0-9]*)$' AND
-        decimal_cmp(user_data_128, '0') IN (0, 1) AND
         decimal_cmp(user_data_128, '340282366920938463463374607431768211455') IN (-1, 0)
     ),
     user_data_64 TEXT NOT NULL CHECK (
         user_data_128 REGEXP '^(0|[1-9][0-9]*)$' AND
-        decimal_cmp(user_data_128, '0') IN (0, 1) AND
         decimal_cmp(user_data_128, '18446744073709551615') IN (-1, 0)
     ),
     user_data_32 INTEGER NOT NULL CHECK (user_data_32 BETWEEN 0 AND 4294967295),
     ledger INTEGER NOT NULL CHECK (ledger BETWEEN 0 AND 4294967295),
     code INTEGER NOT NULL CHECK (code BETWEEN 0 AND 65535),
-    timestamp TEXT NOT NULL UNIQUE DEFAULT (decimal(unixepoch('subsec') * 1000000)) CHECK (
+    timestamp TEXT NOT NULL UNIQUE DEFAULT (decimal_mul(unixepoch('subsec'), 1000000)) CHECK (
         timestamp REGEXP '^(0|[1-9][0-9]*)$' AND
-        decimal_cmp(timestamp, '0') IN (0, 1) AND
         decimal_cmp(timestamp, '18446744073709551615') IN (-1, 0)
         -- make sure timestamp is less than or equal to current time
     ),
@@ -176,16 +163,21 @@ END;
 
 CREATE TRIGGER IF NOT EXISTS after_create_transfer AFTER INSERT ON transfers
 BEGIN
-    UPDATE accounts SET debits_posted = decimal_add(debits_posted, NEW.amount) WHERE id = NEW.debit_account_id;
-    UPDATE accounts SET credits_posted = decimal_add(credits_posted, NEW.amount) WHERE id = NEW.credit_account_id;
+    UPDATE accounts
+        SET debits_posted = decimal_add(debits_posted, NEW.amount)
+        WHERE id = NEW.debit_account_id;
+
+    UPDATE accounts
+        SET credits_posted = decimal_add(credits_posted, NEW.amount)
+        WHERE id = NEW.credit_account_id;
 END;
 
 CREATE TRIGGER IF NOT EXISTS prevent_update_on_transfers BEFORE UPDATE ON transfers
 BEGIN
-    SELECT CASE WHEN true THEN RAISE(ABORT, "transfers cannot be updated") END;
+    SELECT CASE WHEN true THEN RAISE(ABORT, "transfers_cannot_be_modified") END;
 END;
 
 CREATE TRIGGER IF NOT EXISTS prevent_delete_on_transfers BEFORE DELETE ON transfers
 BEGIN
-    SELECT CASE WHEN true THEN RAISE(ABORT, "transfers cannot be deleted") END;
+    SELECT CASE WHEN true THEN RAISE(ABORT, "transfers_cannot_be_deleted") END;
 END;
