74ddcb6bb14e7d989affa526281fe35cc200b98c
Ryan Kroon <rykroon.tech@gmail.com>
2025-12-29 13:53:13 -0500

    Removed print statement.

--- id.go
+++ id.go
@@ -1,14 +1,10 @@
 package turtlebunny
 
 import (
-	"fmt"
-
 	ulid "github.com/oklog/ulid/v2"
 	"lukechampine.com/uint128"
 )
 
 func ID() uint128.Uint128 {
-	id := ulid.Make()
-	fmt.Println(id.String())
-	return uint128.FromBytesBE(id.Bytes())
+	return uint128.FromBytesBE(ulid.Make().Bytes())
 }
